78ff98bc37b1227010172bbefbb942a85b4f35a0
h.jamali <hamed.jamali.soft@gmail.com>
2023-02-12 14:07:59 +0330

    added width percentage size for full size in Paragraph persian itext

--- src/main/java/com/example/report/bildung/ReportV2.java
+++ src/main/java/com/example/report/bildung/ReportV2.java
@@ -10,6 +10,7 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.pdf.languages.ArabicLigaturizer;
 import com.itextpdf.text.pdf.languages.LanguageProcessor;
+import lombok.SneakyThrows;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,45 +20,48 @@ public class ReportV2 {
 
 
     public static void main(String[] args) {
-        Document doc = new Document(PageSize.A4, 36, 36, 90, 36);
-        try {
-
-
-            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("samplev2.pdf"));
-
-
-
-            doc.open();
-            Paragraph p = new Paragraph();
-            BaseFont myFont = BaseFont.createFont("B-NAZANIN.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-            Font paraFont= new Font(myFont, 12);
-            p.setSpacingBefore(5);
-            p.setSpacingAfter(5);
-            p.setAlignment(Paragraph.ALIGN_RIGHT);
-            p.setFont(paraFont);
-
-            String sample = "ﻣﻬﻢ ﻧﻴﺴﺖ ﭼﻘﺪﺭ ﻣﻰ ﻣﺎﻧﻰ، ﻳﮏ ﺭﻭﺯ، ﻳﮏ ﻣﺎﻩ یا ﻳﮏ ﺳﺎﻝ. ﻣﻬﻢ ﮐﻴﻔﻴﺖ ﻣﺎﻧﺪﻥ ﺍﺳﺖ. ﺑﻌﻀﻰ ﻫﺎ ﺩﺭ ﻳﮏ ﺭﻭﺯ" +
-                    "ﺗﻤﺎﻡ ﺩﻧﻴﺎ ﺭﺍ ﺑﻪ ﺗﻮ ﻫﺪﻳﻪ ﻣﻰ ﺩﻫﻨﺪ. ﮔﺎﻫﻰ ﺑﻌﻀﻰ ﻫﺎ، ﻳﮏ ﻋﻤﺮ ﮐﻨﺎﺭﺕ ﻫﺴﺘﻨﺪ ﺍﻣﺎ ﺟﺰ ﺩﺭﺩ، ﻫﻴﭻ ﭼﻴﺰ ﺑﺮﺍﻳﺖ" +
-                    "ندارند، ﻭ ﺗﺎ ﺗﻪ ﺭﻭﺣﺖ ﺭﺍ ﻣﻰ ﺧﺮﺍﺷﻨد! ﺑﻌﻀﻰ ﻫﺎ ﻧﺎﺏ ﻫﺴﺘﻨﺪ! ﻭ ﺑﻪ ﺗﻮ ﻟﺤﻈﻪ ﻫﺎﻯ ﻧﺎﺏ ﺗﺮﻯ ﻫﺪﻳﻪ ﻣﻰ ﺩﻫﻨﺪ!" +
-                    "ﺍﻳﻦ ﺑﻌﻀﻰ ﻫﺎ ﻣﻬﻢ ﻧﻴﺴﺖ ﭼﻘﺪﺭ ﺑﻤﺎﻧﻨﺪ، ﻫﺮ ﭼﻘﺪﺭ ﻫﻢ ﮐﻪ ﺯﻭﺩ ﺑﺮﻭﻧﺪ ﻳﺎﺩﺷﺎﻥ و ﺣﺲ ﺧﻮﺏ ﺑﻮﺩﻧﺸﺎﻥ ﺗﺎ ﻫﻤﻴﺸﻪ ﻫﺴﺖ…";
-//            p.add("متن فارسی");
-            p.add(sample);
-
-            PdfPTable table = new PdfPTable(1);
-            PdfPCell cell = new PdfPCell(p);
-            cell.setBorder(0);
-            cell.setRunDirection(PdfWriter.RUN_DIRECTION_RTL);
-            cell.setHorizontalAlignment(3);
-            table.addCell(cell);
-
-            doc.add(table);
-
-        } catch (DocumentException de) {
-            System.out.println("PDFCreator" + "DocumentException:" + de);
-        } catch (IOException e) {
-
-        } finally {
-
-            doc.close();
+        createPdf();
     }
-}}
+
+    @SneakyThrows
+    private static void createPdf() {
+        Document doc = new Document(PageSize.A4, 36, 36, 90, 36);
+        PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("samplev2.pdf"));
+        // add header and footer
+        HeaderFooterPageEvent event = new HeaderFooterPageEvent();
+        writer.setPageEvent(event);
+        doc.open();
+        String sample = "ﻣﻬﻢ ﻧﻴﺴﺖ ﭼﻘﺪﺭ ﻣﻰ ﻣﺎﻧﻰ، ﻳﮏ ﺭﻭﺯ، ﻳﮏ ﻣﺎﻩ یا ﻳﮏ ﺳﺎﻝ. ﻣﻬﻢ ﮐﻴﻔﻴﺖ ﻣﺎﻧﺪﻥ ﺍﺳﺖ. ﺑﻌﻀﻰ ﻫﺎ ﺩﺭ ﻳﮏ ﺭﻭﺯ" +
+                "ﺗﻤﺎﻡ ﺩﻧﻴﺎ ﺭﺍ ﺑﻪ ﺗﻮ ﻫﺪﻳﻪ ﻣﻰ ﺩﻫﻨﺪ. ﮔﺎﻫﻰ ﺑﻌﻀﻰ ﻫﺎ، ﻳﮏ ﻋﻤﺮ ﮐﻨﺎﺭﺕ ﻫﺴﺘﻨﺪ ﺍﻣﺎ ﺟﺰ ﺩﺭﺩ، ﻫﻴﭻ ﭼﻴﺰ ﺑﺮﺍﻳﺖ" +
+                "ندارند، ﻭ ﺗﺎ ﺗﻪ ﺭﻭﺣﺖ ﺭﺍ ﻣﻰ ﺧﺮﺍﺷﻨد! ﺑﻌﻀﻰ ﻫﺎ ﻧﺎﺏ ﻫﺴﺘﻨﺪ! ﻭ ﺑﻪ ﺗﻮ ﻟﺤﻈﻪ ﻫﺎﻯ ﻧﺎﺏ ﺗﺮﻯ ﻫﺪﻳﻪ ﻣﻰ ﺩﻫﻨﺪ!" +
+                "ﺍﻳﻦ ﺑﻌﻀﻰ ﻫﺎ ﻣﻬﻢ ﻧﻴﺴﺖ ﭼﻘﺪﺭ ﺑﻤﺎﻧﻨﺪ، ﻫﺮ ﭼﻘﺪﺭ ﻫﻢ ﮐﻪ ﺯﻭﺩ ﺑﺮﻭﻧﺪ ﻳﺎﺩﺷﺎﻥ و ﺣﺲ ﺧﻮﺏ ﺑﻮﺩﻧﺸﺎﻥ ﺗﺎ ﻫﻤﻴﺸﻪ ﻫﺴﺖ…";
+        PdfPTable element = newPersianParagraph(sample);
+         doc.add(element);
+        doc.close();
+
+    }
+
+    @SneakyThrows
+    private static PdfPTable newPersianParagraph(String text) {
+        Paragraph p = new Paragraph();
+        BaseFont myFont = BaseFont.createFont("B-NAZANIN.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        Font paraFont = new Font(myFont, 12);
+        p.setSpacingBefore(5);
+        p.setSpacingAfter(5);
+        p.setAlignment(Paragraph.ALIGN_RIGHT);
+        p.setFont(paraFont);
+
+        p.add(text);
+        PdfPTable table = new PdfPTable(1);
+
+        table.setWidthPercentage(100);
+        PdfPCell cell = new PdfPCell(p);
+        cell.setBorder(0);
+        cell.setRunDirection(PdfWriter.RUN_DIRECTION_RTL);
+        cell.setHorizontalAlignment(Paragraph.ALIGN_JUSTIFIED);
+        table.addCell(cell);
+        return table;
+    }
+
+
+}
