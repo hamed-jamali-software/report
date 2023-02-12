28661792924ec2a41f253e90d16dea17721802af
h.jamali <hamed.jamali.soft@gmail.com>
2023-02-12 12:05:41 +0330

    Initial commit

---
+++ src/main/java/com/example/report/bildung/InsertTextInPdf.java
@@ -0,0 +1,25 @@
+package com.example.report.bildung;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfWriter;
+import lombok.SneakyThrows;
+
+import java.io.FileOutputStream;
+
+public class InserTextInPdf {
+    @SneakyThrows
+    public Document pdfFile(){
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk("Hello World", font);
+
+        document.add(chunk);
+        document.close();
+    }
+}
